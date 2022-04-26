b0145cb7446ddcb916466a71637080e9285126ad
Aryan Kumar <meetaryankumar@gmail.com>
2022-04-26 21:30:22 +0530

    FirstCommit

---
+++ config.py
@@ -0,0 +1,12 @@
+import random
+from os import environ
+
+DEFAULT_PREFIX = ">" # the bot's default prefix
+BOT_TOKEN = environ["TOKEN"] # your bot's token
+DATABASE_URI = environ["MONGO"] # MongoDB URI, you can get this once you set it up
+DATABASE_NAME = "collectionbot" # name of database
+DBL_TOKEN = "" # Top.gg token - not required
+
+def RATES(bet):
+	if (true):
+		return true
